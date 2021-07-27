ca64e750cd84213e1893639ddd94f89e604318b2
huamingming <13588317074@163.com>
2021-07-27 16:52:10 +0800

    第一次提交

---
+++ dreamland-web/src/main/java/wang/dreamland/www/dao/ResourceMapper.java
@@ -0,0 +1,17 @@
+package wang.dreamland.www.dao;
+
+import wang.dreamland.www.entity.Resource;
+
+public interface ResourceMapper {
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Resource record);
+
+    int insertSelective(Resource record);
+
+    Resource selectByPrimaryKey(Long id);
+
+    int updateByPrimaryKeySelective(Resource record);
+
+    int updateByPrimaryKey(Resource record);
+}
---
+++ dreamland-web/src/main/webapp/index.jsp
@@ -0,0 +1,7 @@
+<%@ page contentType="text/html;charset=UTF-8"%>
+<html>
+<body>
+<h2>点我注册</h2>
+<a href="register.jsp">点我注册</a>
+</body>
+</html>
