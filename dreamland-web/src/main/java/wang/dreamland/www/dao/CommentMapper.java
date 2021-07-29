3929445c205b3d9ffe0cc57f91490d0cb12a2996
huamingming <13588317074@163.com>
2021-07-29 13:35:24 +0800

    注册jsp更新

--- dreamland-web/src/main/java/wang/dreamland/www/dao/CommentMapper.java
+++ dreamland-web/src/main/java/wang/dreamland/www/dao/CommentMapper.java
@@ -1,10 +1,7 @@
 package wang.dreamland.www.dao;
 
-import org.apache.ibatis.annotations.Param;
 import tk.mybatis.mapper.common.Mapper;
 import wang.dreamland.www.entity.Comment;
-
-import java.util.List;
 
 /**
  * Created by Administrator on 2018/2/27.
